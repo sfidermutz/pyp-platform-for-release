9197189ef1513204adf415f34a249f85efc9f904
sfidermutz <sfidermutz@icloud.com>
2025-11-30 10:24:58 +0100

    Create step2_core_tables.sql

---
+++ sql/step2_core_tables.sql
@@ -0,0 +1,6 @@
+git checkout main
+git pull origin main
+# create file sql/step2_core_tables.sql with the provided SQL
+git add sql/step2_core_tables.sql
+git commit -m "chore(sql): add step2_core_tables migration (tokens/modules/reflections/... )"
+git push origin main
