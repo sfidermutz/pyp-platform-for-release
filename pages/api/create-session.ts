1c683df971545f364ee227844a247074fca1a854
sfidermutz <sfidermutz@icloud.com>
2025-12-23 19:23:06 +0100

    Import supabaseAdmin in create-session API

--- pages/api/create-session.ts
+++ pages/api/create-session.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
 import { serialize } from "cookie";
+import { supabaseAdmin } from '../../lib/supabaseAdmin';
+
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
